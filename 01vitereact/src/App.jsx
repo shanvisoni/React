579d483312e5d55447854dce803993b798d89fb7
Shanvi <shanvisoni7388@gmail.com>
2024-06-14 00:20:04 +0530

    Basic to practicle

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import  Chai from "./chai"
+const userName="Shanvi soni"
+function App(){
+  return (
+    <>
+   
+   <h2>Hello guys welcom in microsoft {userName} </h2>
+   <Chai />
+     </>
+  )
+
+}
+
+export default App
